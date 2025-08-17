468f574b6047f1b5d65c9ffa8038cd83d6633a70
Daulét <dauletanigylator@gmail.com>
2025-08-17 14:22:21 +0200

    Initial Commit

---
+++ Meal&Deal/Meal_DealApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_DealApp.swift
+//  Meal&Deal
+//
+//  Created by Daulet Yerkinov on 17.08.25.
+//
+
+import SwiftUI
+
+@main
+struct Meal_DealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Meal&DealTests/Meal_DealTests.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_DealTests.swift
+//  Meal&DealTests
+//
+//  Created by Daulet Yerkinov on 17.08.25.
+//
+
+import Testing
+@testable import Meal_Deal
+
+struct Meal_DealTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
